ed36b37607d8845120a949d3850fe9b2ce81e57c
1hasanovv <abdurahmonpubg218@gmail.com>
2024-07-29 22:36:40 +0500

    finish

---
+++ js/main.js
@@ -0,0 +1,76 @@
+
+
+var question1 = "Ismingiz nma?"
+
+var answer1 = prompt(question1)
+
+
+
+var question2 = "Familangiz nma?"
+
+var answer2 = prompt(question2)
+
+
+var question3 = "Nechi yoshdasiz?"
+
+var answer3 = prompt(question3)
+
+
+var question4 = "Qayerda oqisiz?"
+
+var answer4 = prompt(question4)
+
+
+var question5 = "Nmalarga qiziqasiz?"
+
+var answer5 = prompt(question5)
+
+
+var question6 = "HTML sozini toliq yozilishi qanqa?"
+
+var answer6 = prompt(question6)
+
+
+var question7 = "Css nma?"
+
+var answer7 = prompt(question7)
+
+
+var question8 = "Css ni nechi hil usulda ulasa boladi?"
+
+var answer8 = prompt(question8)
+
+
+var question9 = "Figma nma?"
+
+var answer9 = prompt(question9)
+
+
+var question10 = "3 oy ichida neshta amaliyot qildingiz?"
+
+var answer10 = prompt(question10)
+
+
+var question11 = "Git Hub nma?"
+
+var answer11 = prompt(question11)
+
+
+var question12 = "Git Hubda nmalar qilsa boladi?"
+
+var answer12 = prompt(question12)
+
+
+var question13 = "Max widht nma?"
+
+var answer13 = prompt(question13)
+
+
+var question14 = "Mini widht nma?"
+
+var answer14 = prompt(question14)
+
+
+var question15 = "responsiv  nma?"
+
+var answer15 = prompt(question15)
